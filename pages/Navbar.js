a252a3989670d24878a54a4df1bd1d4bd0f47e11
문기현 <john7266@naver.com>
2023-09-14 12:19:06 +0900

    AlphaBuild

---
+++ pages/Navbar.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/Home.module.css'
+
+export default function Navbar() {
+    return (
+        <div className={styles.navbar}>
+            <a href="./">Home</a> | 
+            <a href="./Chapter2_1"> CH2_1</a> | 
+            <a href="./Chapter2_2"> CH2_2</a> | 
+        </div>
+    )    
+}
--- pages/index copy.js
+++ pages/index copy.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Navbar from './Navbar'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -38,7 +38,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-        <Navbar />
+
         <div className={styles.center}>
           <Image
             className={styles.logo}
